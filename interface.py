b6e3d880d8e453045a5cc5f6993f7fcca32f237e
91-julian-sanchez <91.julian.sanchez@gmail.com>
2023-04-20 19:29:28 -0500

    feat: add interactive interface for selecting question types

---
+++ interface.py
@@ -0,0 +1,33 @@
+from inquirer import prompt, Checkbox
+from rich.console import Console
+
+console = Console()
+console.print("[italic] ¡Hola! Gracias por venir a esta entrevista. [/italic]😊\n ")
+choices_dict = {
+    'yes_no': 'Preguntas cerradas de "sí o no" ✅',
+    'multiple_choice': 'Preguntas de opción múltiple con única respuesta 🔘',
+    'short_answer': 'Preguntas abiertas de respuesta corta ✏️'
+}
+
+def select_questions():
+    questions = [
+        Checkbox('preguntas',
+                message='Selecciona el tipo de preguntas que prefieres responder',
+                choices= [(v, k) for k, v in choices_dict.items()])
+    ]
+    answers = prompt(questions)
+    if not answers['preguntas']:
+        return False
+    return answers['preguntas']
+
+while True:
+    selected_options = select_questions()
+    if not selected_options:
+        console.print("❌ [bold red]Error: selecciona una opción válida.[/bold red]\n ")
+        continue
+    else:
+        break
+
+console.print("[green]👍 Opciones seleccionadas: [/green]")
+for opcion in selected_options:
+    console.print(f"  - {choices_dict[opcion]} ")
