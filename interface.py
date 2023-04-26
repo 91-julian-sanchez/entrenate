e67bd6ffd43019056cdfa8a201bf3c5f004036ff
91-julian-sanchez <91.julian.sanchez@gmail.com>
2023-04-26 00:11:40 -0500

    chore: add exception handling for prompts input from interface

--- interface.py
+++ interface.py
@@ -44,8 +44,12 @@ def prompt_yes_or_no_answer() -> str:
                     choices=['Si', 'No'],
                 ),
     ]
-    answers = inquirer.prompt(questions)
-    return answers['yes_or_no']
+    try:
+        answers = inquirer.prompt(questions)
+        return answers['yes_or_no']
+    except KeyError:
+        console.print("❌ [bold red]Error: seleccione una opción válida ('Si' o 'No').[/bold red]\n ")
+        return prompt_yes_or_no_answer()
 
 def prompt_multiple_choice_answer() -> str:
     questions = [
@@ -54,15 +58,31 @@ def prompt_multiple_choice_answer() -> str:
                     choices=['A', 'B', 'C', 'D'],
                 ),
     ]
-    answers = inquirer.prompt(questions)
-    return answers['multiple_choice']
+    try:
+        answers = inquirer.prompt(questions)
+        return answers['multiple_choice']
+    except KeyError:
+        console.print("❌ [bold red]Error: seleccione una opción válida (A, B, C o D).[/bold red]\n ")
+        return prompt_multiple_choice_answer()
 
 def prompt_short_answer() -> str:
-    questions = [
-        inquirer.Text('short_answer', message="?"),
-    ]
-    answers = inquirer.prompt(questions)
-    return answers['short_answer']
+    while True:
+        try:
+            questions = [
+                inquirer.Text('short_answer', message="?"),
+            ]
+            answers = inquirer.prompt(questions)
+            short_answer = answers['short_answer'].strip()  # elimina espacios en blanco al principio y al final
+            if len(short_answer) == 0:
+                raise ValueError("La respuesta no puede estar vacía.")
+            elif len(short_answer) > 100:
+                raise ValueError("La respuesta es demasiado larga (máximo 100 caracteres).")
+            return short_answer
+        except (KeyError, ValueError) as e:
+            console.print(f"❌ [bold red]Error: {e} Por favor, inténtelo de nuevo.[/bold red]\n ")
 
 if __name__ == '__main__':
     print(interfaz_select_question_types())
+    print(prompt_yes_or_no_answer())
+    print(prompt_multiple_choice_answer())
+    print(prompt_short_answer())
