131e7050d34898d832d053b32291b912e7083d22
91-julian-sanchez <91.julian.sanchez@gmail.com>
2023-04-25 23:51:30 -0500

    feat: send question answer to gpt and print answer from assistant

--- interface.py
+++ interface.py
@@ -1,10 +1,11 @@
+import inquirer
 from inquirer import prompt, Checkbox
 from rich.console import Console
 
 console = Console()
 console.print("[italic] ¡Hola! Gracias por venir a esta entrevista. [/italic]😊\n ")
 choices_dict = {
-    'yes_no': 'Preguntas cerradas de "sí o no" ✅',
+    'yes_or_no': 'Preguntas cerradas de "sí o no" ✅',
     'multiple_choice': 'Preguntas de opción múltiple con única respuesta 🔘',
     'short_answer': 'Preguntas abiertas de respuesta corta ✏️'
 }
@@ -35,6 +36,30 @@ def interfaz_select_question_types():
     console.print("\n")
 
     return selected_options
+
+def prompt_yes_or_no_answer() -> str:
+    questions = [
+    inquirer.List('yes_or_no',
+                    message="Tu respuesta es",
+                    choices=['Si', 'No'],
+                ),
+    ]
+    answers = inquirer.prompt(questions)
+    return answers['yes_or_no']
+
+def prompt_multiple_choice_answer() -> str:
+    questions = [
+    inquirer.List('multiple_choice',
+                    message="Tu respuesta es",
+                    choices=['A', 'B', 'C', 'D'],
+                ),
+    ]
+    answers = inquirer.prompt(questions)
+    return answers['multiple_choice']
+
+def prompt_short_answer() -> str:
+    prompt = input()
+    return prompt
 
 if __name__ == '__main__':
     print(interfaz_select_question_types())
--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import os
 from dotenv import load_dotenv
 import openai
 from interface import interfaz_select_question_types
+from interface import prompt_yes_or_no_answer
+from interface import prompt_multiple_choice_answer
+from interface import prompt_short_answer
  
 load_dotenv()  # take environment variables from .env.
 lang='Spanish'
@@ -9,7 +12,7 @@ assistant_role='Tech Recruiter'
 user_role='Developer'
 user_level='junior'
 types_questions_dict = {
-    'yes_no': ('Preguntas cerradas de "sí o no" ✅', '"yes or no" option'),
+    'yes_or_no': ('Preguntas cerradas de "sí o no" ✅', '"yes or no" option'),
     'multiple_choice': ('Preguntas de opción múltiple con única respuesta 🔘', 'multiple choice question with only one answer'),
     'short_answer': ('Preguntas abiertas de respuesta corta ✏️', 'open-ended and short answer')
 }
@@ -20,21 +23,40 @@ def system_context():
 def user_question(type_question):
     return f"Can you ask me a question about programming basics?. the question must be a {type_question}"
 
+def __chat_completion_create(messages):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
+    response_content = response.choices[0].message.content
+    print(f"{response_content}\n")
+    return response_content
+    
 def assistant_chatbot(selected_type_question):
+    context = {"role": "system", "content": system_context()}
+    messages = [context]
     for type_question in selected_type_question:
         if os.getenv('ASSISTANT_ENABLED') == 'True':
-            response = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=[
-                        {"role": "system", "content": system_context()},
-                        {"role": "user", "content": user_question(type_question[1])}
-                    ]
-            )
-            response_content = response.choices[0].message.content
-            print(f"{response_content}\n")
+            messages.append({"role": "user", "content": user_question(type_question[1])})
+            response_content = __chat_completion_create(messages)
+            messages.append({"role": "assistant", "content": response_content})
+
+            if type_question[1] in '"yes or no" option':
+                content = prompt_yes_or_no_answer()
+                messages.append({"role": "user", "content": f"la respuesta es {content} ?"})
+                __chat_completion_create(messages)
+            elif type_question[1] in 'multiple choice question with only one answer':
+                content = prompt_multiple_choice_answer()
+                messages.append({"role": "user", "content": f"la respuesta es la opción {content} ?"})
+                __chat_completion_create(messages)
+            else:
+                content = prompt_short_answer()
+                messages.append({"role": "user", "content": f"la respuesta es: {content} ?"})
+                __chat_completion_create(messages)
 
 if __name__ == '__main__':
     openai.api_key = os.getenv("CHATGPT_API_KEY")
 
     selected_question_types = [types_questions_dict[option] for option in interfaz_select_question_types()]
     assistant_chatbot(selected_question_types)
+    
